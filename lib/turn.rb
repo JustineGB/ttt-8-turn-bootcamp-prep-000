5cc4d9ce31920ebd562a0540f6040eca01910cf8
Justine Bricknell <justinegayton@gmail.com>
2018-06-08 16:44:39 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -33,5 +33,7 @@ def input_to_index(user_input)
 def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
-index = input_to_index(user_input)
+  index = input_to_index(user_input)
 end
+
+display_board(board)
