2d5efba47379cc2332517d22ba702b79f4689f7b
Justine Bricknell <justinegayton@gmail.com>
2018-06-08 16:00:49 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,34 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index, value)
+  if index.between?(0, 8) && !position_taken?(board, index)
+    return true
+end
+end
+
+def position_taken?(board, index, value)
+taken = nil
+if board[index] == " " || "" || nil
+  taken = false
+else
+  taken = true
+end
+end
+
+def move(board, index, value)
+     board[index] = value
+   end
+
+def input_to_index(user_input)
+     user_input.to_i - 1
+   end
+
+def turn(board)
+  puts "Please enter 1-9:"
+end
