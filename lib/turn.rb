6087e4f31c6a5da2f08cc70bb17daa87cc9eaa81
Justine Bricknell <justinegayton@gmail.com>
2018-06-08 16:57:06 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -17,7 +17,7 @@ def input_to_index(user_input)
    end
 
 def valid_move?(board, index)
-  if index.between?(0, 8) && !position_taken?(board, index)
+  if (index.between?(0, 8) && !position_taken?(board, index))
     return true
 end
 end
